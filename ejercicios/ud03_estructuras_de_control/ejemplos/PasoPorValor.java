4338ffa1d2d5bbf8b20e65d4554ebd5128d8c746
DVelascoG6 <232549405+DVelascoG6@users.noreply.github.com>
2025-11-12 11:42:47 +0000

    Ejercicios y ejemplos del tema 3

---
+++ ejercicios/ud03_estructuras_de_control/ejemplos/PasoPorValor.java
@@ -0,0 +1,16 @@
+package ejercicios.ud03_estructuras_de_control.ejemplos;
+
+public class PasoPorValor {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("num="+num+"  doble="+doble(num));
+    }
+
+    static int doble(int num)
+    {
+        num*=2;
+        return num;
+    }
+    static int num = 2; 
+}
---
+++ ejercicios/ud03_estructuras_de_control/ejemplos/Triangulo2.java
@@ -0,0 +1,24 @@
+package ejercicios.ud03_estructuras_de_control.ejemplos;
+
+public class Triangulo2 {
+   
+    static void filaTriangulo(int n) {
+        if (n > 0) {
+            System.out.print("* ");
+            filaTriangulo(n - 1);
+        } else
+            System.out.println();
+    }
+
+    static void triangulo(int n) {
+        if (n > 0) {
+            triangulo(n - 1);
+            filaTriangulo(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        triangulo(5);
+    } 
+
+}
